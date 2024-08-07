d05dadf5a39f6f019279fa601e63dbe1a2861844
cuervo-blanco <jaios.228@gmail.com>
2024-08-06 22:11:34 -0400

    Opus frame size fix

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub const FRAME_SIZE: usize = 16; 
-pub const SAMPLE_RATE: f32 = 8000.0;
-pub const CHANNELS: f32 = 2.0;
-pub const BUFFER_SIZE: usize = 512; 
+pub const SAMPLE_RATE: f32 = 48000.0;
+pub const CHANNELS: f32 = 1.0;
+pub const BUFFER_SIZE: usize = 960; 
